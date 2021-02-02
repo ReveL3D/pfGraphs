14378a8fadaf139193f20920910f6538c865a1dd
ReveL3D <neo509@wp.pl>
2021-02-02 16:49:13 +0000

    add attachments list

---
+++ attachments.js
@@ -0,0 +1,127 @@
+var attachments = 
+{
+
+"No_Attachment":
+{
+	"maxDmgMul": 1,
+	"minDmgMul": 1,
+	"maxRangeMul": 1,
+	"minRangeMul": 1,
+	"headMul": 1,
+	"torsoMul": 1,
+	"category": "Other"
+},
+
+"No Attachment":
+{
+	"maxDmgMul": 1,
+	"minDmgMul": 1,
+	"maxRangeMul": 1,
+	"minRangeMul": 1,
+	"headMul": 1,
+	"torsoMul": 1,
+	"category": "Barrel"
+},
+
+"Suppressor":
+{
+	"maxDmgMul": 0.95,
+	"minDmgMul": 1,
+	"maxRangeMul": 0.85,
+	"minRangeMul": 0.85,
+	"headMul": 1,
+	"torsoMul": 1,
+	"category": "Barrel"
+},
+
+"R2 Suppressor":
+{
+	"maxDmgMul": 0.95,
+	"minDmgMul": 0.95,
+	"maxRangeMul": 0.9,
+	"minRangeMul": 0.9,
+	"headMul": 1,
+	"torsoMul": 1,
+	"category": "Barrel"
+},
+
+"ARS Suppressor":
+{
+	"maxDmgMul": 1,
+	"minDmgMul": 0.9,
+	"maxRangeMul": 1,
+	"minRangeMul": 1,
+	"headMul": 1,
+	"torsoMul": 1,
+	"category": "Barrel"
+},
+
+
+"PBS-1 Suppressor":
+{
+	"maxDmgMul": 0.95,
+	"minDmgMul": 0.92,
+	"maxRangeMul": 1,
+	"minRangeMul": 0.85,
+	"headMul": 1,
+	"torsoMul": 1,
+	"category": "Barrel"
+},
+
+
+"PBS-4 Suppressor":
+{
+	"maxDmgMul": 0.98,
+	"minDmgMul": 0.92,
+	"maxRangeMul": 1,
+	"minRangeMul": 0.9,
+	"headMul": 1,
+	"torsoMul": 1,
+	"category": "Barrel"
+},
+
+"Osprey Suppressor":
+{
+	"maxDmgMul": 0.85,
+	"minDmgMul": 1,
+	"maxRangeMul": 1.1,
+	"minRangeMul": 1,
+	"headMul": 1,
+	"torsoMul": 1,
+	"category": "Barrel"
+},
+
+"Sionics Suppressor":
+{
+	"maxDmgMul": 0.8,
+	"minDmgMul": 1,
+	"maxRangeMul": 0.8,
+	"minRangeMul": 1,
+	"headMul": 1,
+	"torsoMul": 1,
+	"category": "Barrel"
+},
+
+"Armor Piercing":
+{
+	"maxDmgMul": 1,
+	"minDmgMul": 1,
+	"maxRangeMul": 0.5,
+	"minRangeMul": 1,
+	"headMul": 1,
+	"torsoMul": 1,
+	"category": "Other"
+},
+
+"Hollow Point":
+{
+	"maxDmgMul": 1.2,
+	"minDmgMul": 0.8,
+	"maxRangeMul": 1.1,
+	"minRangeMul": 0.9,
+	"headMul": 0.8,
+	"torsoMul": 1,
+	"category": "Other"
+},
+
+}
